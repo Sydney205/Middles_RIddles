c813ee1f61c2e8a68c0b1683a904e2ea6bf7965d
Dieke Sydney <101925868+Sydney205@users.noreply.github.com>
2023-01-18 15:50:01 +0000

    simple code lol

--- Middles Riddles_files/script.js
+++ Middles Riddles_files/script.js
@@ -3,3 +3,19 @@ const start = () => {
    window.location.href = 'Middles Riddles_files/riddles.html';
 }
 
+// The Correct Answer
+const correctAnswer = 'footstep';
+
+// The user's answer
+const userAnswer = (e) => {
+   e.target.value;
+}
+
+//onSubmit...
+const submit = () => {
+   if (userAnswer === correctAnswer) {
+      alert('correct'); // would change this later to something better
+   } else {
+      alert('wrong');
+   }
+}
