39f8f02309439783163072bf5caa4c302e13cd6f
Dieke Sydney <101925868+Sydney205@users.noreply.github.com>
2023-01-18 15:39:24 +0000

    Update script.js

--- Middles Riddles_files/script.js
+++ Middles Riddles_files/script.js
@@ -2,21 +2,5 @@ const start = () => {
    window.location.href = 'Middles Riddles_files/riddles.html';
 }
 
-// The Answer
-const answer = (e) => {
- e.target.value;
-}
-
-const correctAnswer = 'footstep';
-
-const inputValue = document.getElementById('answer');
-
-const submit = () => {
-  if (inputValue.value == correctAnswer) {
-   window.location.href = 'success.html';
- } else {
-  alert('Wrong');
- }
-}
 
 
