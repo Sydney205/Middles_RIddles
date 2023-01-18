adba96c4d3d94fc43602d554261215b58c622d20
Dieke Sydney <101925868+Sydney205@users.noreply.github.com>
2023-01-18 15:54:09 +0000

    Update script.js

--- Middles Riddles_files/script.js
+++ Middles Riddles_files/script.js
@@ -6,16 +6,23 @@ const start = () => {
 // The Correct Answer
 const correctAnswer = 'footstep';
 
-// The user's answer
-const userAnswer = (e) => {
+// The user's answer...
+
+// The input box
+const box = document.getElementById('answer');
+
+const onInputChange = (e) => {
    e.target.value;
 }
 
 //onSubmit...
 const submit = () => {
-   if (userAnswer === correctAnswer) {
+   if (box.value == correctAnswer) {
       alert('correct'); // would change this later to something better
    } else {
       alert('wrong');
    }
 }
+
+
+// Just a simple code for now... it might look to simple now but might get complex later LOL 
